a28ae7b611c0fcd701602b209ec0fb65fce245f8
TomoyaOgashiwa <tomoya07oga@gmail.com>
2025-04-21 00:53:06 -0700

    feat: add functionality to display hourly weather data every three hours and update date formatting

--- src/utils/date.js
+++ src/utils/date.js
@@ -6,6 +6,13 @@ export const formatDateToYYYYMMDD = (date) => {
 	return `${yyyy}-${mm}-${dd}`;
 };
 
+export const formatDateToMMDD = (date) => {
+	const mm = String(date.getMonth() + 1).padStart(2, "0"); // Months are 0-based
+	const dd = String(date.getDate()).padStart(2, "0");
+
+	return `${mm}-${dd}`;
+};
+
 export const formatHour = (hour) => {
 	if (hour < 10) {
 		return `0${hour}:00`;
