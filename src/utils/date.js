842a3809234d6018009980e194be70a0d06f88b1
TomoyaOgashiwa <tomoya07oga@gmail.com>
2025-04-21 10:46:48 -0700

    fix: correct date formatting in formatDateToMMDD function

--- src/utils/date.js
+++ src/utils/date.js
@@ -8,7 +8,7 @@ export const formatDateToYYYYMMDD = (date) => {
 
 export const formatDateToMMDD = (date) => {
 	const mm = String(date.getMonth() + 1).padStart(2, "0"); // Months are 0-based
-	const dd = String(date.getDate()).padStart(2, "0");
+	const dd = String(date.getDate() + 1).padStart(2, "0");
 
 	return `${mm}-${dd}`;
 };
